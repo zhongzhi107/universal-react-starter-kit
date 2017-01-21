d830ad22559f8ee261004fdb776f606628601f1c
zhongzhi107 <zhong.zhi@163.com>
2017-01-21 17:23:29 +0800

    fix: --tags

--- npm-scripts/prepublish.js
+++ npm-scripts/prepublish.js
@@ -4,6 +4,6 @@ import {execSync} from 'child_process';
 import fs from 'fs';
 import pkg from '../package.json';
 
-const cmd = `git tag v${pkg.version} && git push origin -tags`;
+const cmd = `git tag v${pkg.version} && git push origin --tags`;
 console.log(cmd);
 execSync(cmd);
