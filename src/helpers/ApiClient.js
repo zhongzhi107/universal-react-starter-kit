fd3faa7a98fba9aa54c1c31dc3d6b7cd405350da
zhongzhi107 <zhong.zhi@163.com>
2017-01-23 15:31:17 +0800

    style: ES6 code style

--- src/helpers/ApiClient.js
+++ src/helpers/ApiClient.js
@@ -1,5 +1,5 @@
 import superagent from 'superagent';
-import config from '../config';
+import {apiHost, apiPort} from 'config';
 
 const methods = ['get', 'post', 'put', 'patch', 'del'];
 
@@ -7,10 +7,10 @@ function formatUrl(path) {
   const adjustedPath = path[0] !== '/' ? '/' + path : path;
   if (__SERVER__) {
     // Prepend host and port of the API server to the path.
-    return 'http://' + config.apiHost + ':' + config.apiPort + adjustedPath;
+    return `http://${apiHost}:${apiPort}${adjustedPath}`;
   }
   // Prepend `/api` to relative URL, to proxy to API server.
-  return '/api' + adjustedPath;
+  return `/api${adjustedPath}`;
 }
 
 export default class ApiClient {
--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -37,8 +37,14 @@ export default class Html extends Component {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           {/* styles (will be present only in production with webpack extract text plugin) */}
           {Object.keys(assets.styles).map((style, key) =>
-            <link href={assets.styles[style]} key={key} media="screen, projection"
-                  rel="stylesheet" type="text/css" charSet="UTF-8"/>
+            <link
+              href={assets.styles[style]}
+              key={key}
+              media="screen, projection"
+              rel="stylesheet"
+              type="text/css"
+              charSet="UTF-8"
+            />
           )}
 
           {/* (will be present only in development mode) */}
