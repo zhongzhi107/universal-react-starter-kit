d8707a09845cb6c21c19f74b05718b01c518a862
zhongzhi107 <zhong.zhi@163.com>
2017-02-07 22:29:31 +0800

    feat: Replace express with koa2

--- api/api.js
+++ api/api.js
@@ -1,54 +1,41 @@
-import express from 'express';
-import session from 'express-session';
-import bodyParser from 'body-parser';
-import config from '../src/config';
-import * as actions from './actions/index';
-import {mapUrl} from 'utils/url.js';
+import Koa from 'koa';
+import convert from 'koa-convert';
+import session from 'koa-session';
+import body from 'koa-body';
 import PrettyError from 'pretty-error';
-import captcha from 'svg-captcha';
+import {mapUrl} from 'utils/url';
+import * as actions from 'actions/index';
+import config from '../src/config';
 
 const pretty = new PrettyError();
-const app = express();
+const app = new Koa();
 
-app.use(session({
-  secret: 'react and redux rule!!!!',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { maxAge: 60000 }
-}));
-app.use(bodyParser.json());
+app.use(convert(session(app)));
+app.use(body());
 
-app.get('/captcha', (req, res) => {
-  const png = captcha.create();
-  req.session.captcha = png.text;
-
-  res.set('Content-Type', 'image/svg+xml');
-  res.status(200).send(png.data);
-});
-
-app.use((req, res) => {
-  const splittedUrlPath = req.url.split('?')[0].split('/').slice(1);
-
-  const {action, params} = mapUrl(actions, splittedUrlPath);
+app.use(async (ctx) => {
+  const splittedUrlPath = ctx.url.split('?')[0].split('/').slice(1);
+  const { action, params } = mapUrl(actions, splittedUrlPath);
 
   if (action) {
-    action(req, params)
-      .then((result) => {
-        if (result instanceof Function) {
-          result(res);
-        } else {
-          res.json(result);
-        }
-      }, (reason) => {
-        if (reason && reason.redirect) {
-          res.redirect(reason.redirect);
-        } else {
-          console.error('API ERROR:', pretty.render(reason));
-          res.status(reason.status || 500).json(reason);
-        }
-      });
+    try {
+      const result = await action(ctx, params);
+      if (result instanceof Function) {
+        result(ctx);
+      } else {
+        ctx.body = JSON.stringify(result);
+      }
+    } catch (error) {
+      if (error && error.redirect) {
+        ctx.redirect(error.redirect);
+      } else {
+        console.error('API ERROR: ', pretty.render(error));
+        ctx.status = error.status || 500;
+        ctx.body = JSON.stringify(error);
+      }
+    }
   } else {
-    res.status(404).end('NOT FOUND');
+    ctx.status = 400;
   }
 });
 
