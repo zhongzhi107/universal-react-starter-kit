e029810bb9c2ace4ae7fd4c4fdb91ed25f479451
zhongzhi107 <zhong.zhi@163.com>
2017-02-14 11:07:08 +0800

    feat: Add some meta elements for PWA

--- src/config/helmet.js
+++ src/config/helmet.js
@@ -1,5 +1,6 @@
+const projectName = 'universal-react-starter-kit';
 export default Object.freeze({
-  title: 'universal-react-starter-kit',
+  title: projectName,
   description: 'All the modern best practices in one example.',
   head: {
     htmlAttributes: {
@@ -10,10 +11,19 @@ export default Object.freeze({
       { charset: 'utf-8' },
       { name: 'description', content: 'All the modern best practices in one example.' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'theme-color', content: '#2196F3' }
+      { name: 'theme-color', content: '#2196F3' },
+      { name: 'mobile-web-app-capable', content: 'yes' },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      { name: 'application-name', content: projectName },
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+      { name: 'apple-mobile-web-app-title', content: projectName },
+      { name: 'msapplication-TileImage', content: '/images/touch/logo_144.png' },
+      { name: 'msapplication-TileColor', content: '#2196F3' }
     ],
     link: [
+      // { rel: 'canonical', href: 'https://xxx.com/' },
       { rel: 'shortcut icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', href: '/images/touch/logo_192.png' },
       { rel: 'manifest', href: '/manifest.json' }
     ],
     script: [],
