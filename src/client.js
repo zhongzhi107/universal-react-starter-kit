d821791a69fbc4a61da5158bb85ddc18f331cc05
zhongzhi107 <zhong.zhi@163.com>
2017-02-14 12:48:34 +0800

    docs: Change text

--- src/client.js
+++ src/client.js
@@ -47,7 +47,10 @@ if (process.env.NODE_ENV !== 'production') {
   window.React = React; // enable debugger
 
   if (!dest || !dest.firstChild || !dest.firstChild.attributes || !dest.firstChild.attributes['data-react-checksum']) {
-    console.error('Server-side React render was discarded. Make sure that your initial render does not contain any client-side code.');
+    console.error(
+      'Server-side React render was discarded.',
+      'Make sure that your initial render does not contain any client-side code.'
+    );
   }
 }
 
