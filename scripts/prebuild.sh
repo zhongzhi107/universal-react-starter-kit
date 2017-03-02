514d81131c2d725db353331e49aaf0753167e78e
zhongzhi107 <zhong.zhi@163.com>
2017-03-02 16:46:13 +0800

    debug

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,17 +1,17 @@
-git remote add touch_server git@gitlab.corp.qunar.com:mobile/touch_server.git
+git remote add touch_client git@gitlab.corp.qunar.com:fe/touch_client.git
 
-git fetch touch_server
+git fetch touch_client
 
-diff=`git diff origin/init touch_server/init --name-only`
+diff=`git diff origin/init touch_client/init --name-only`
 
 echo "diff: ${diff}"
 
 if [ ! $diff ]; then
+  echo "diff结果：client库和server库代码完全一致"
   git remote remove touch_server
   exit 2
 else
-  echo "两个仓库的代码不一致，请同步后再发布"
-  echo "存在差异的文件列表如下："
+  echo "diff结果：两个仓库的代码不一致，请同步后再发布。/n存在差异的文件列表如下："
   echo "${diff}"
   exit 1
 fi
