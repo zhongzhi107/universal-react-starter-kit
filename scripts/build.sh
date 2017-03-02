b653a67f1669d49bd44a9d3068e6f9f699a20fe0
zhongzhi107 <zhong.zhi@163.com>
2017-03-02 11:29:25 +0800

    ci: Rename build.client.sh to build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-# 该文件是前端编译使用的脚本，后端工程不需要
+# 该文件是前后端通用的编译使用的脚本
 
 export PATH=/usr/local/n/versions/node/6.2.1/bin:$PATH
 
 if [ ! -n "$1" ] ;then
   echo 参数错误
-  echo usage: build.sh cdn_root
+  echo usage: build.client.sh <cdn_root>
 else
   echo hostname: `hostname`
   echo node version: `node -v`
   echo npm version: `npm -v`
   echo CDN_ROOT: $1
   # npm-cache install --cacheDirectory /home/q/prj/npm/fe/dzs_touch --clearInvalidCache true npm --registry http://registry.npm.corp.qunar.com --disturl=https://npm.taobao.org/dist --sass-binary-site=http://npm.taobao.org/mirrors/node-sass --production
+
+  # Jenkins 会为前端工程安装好依赖包，后端工程需要手动装
+  if [ ! -d "node_modules" ]; then
+    npm --registry http://registry.npm.corp.qunar.com --disturl=https://npm.taobao.org/dist --sass-binary-site=http://npm.taobao.org/mirrors/node-sass --production
+  fi
+
   CDN_ROOT=$1 npm run build
-fio
+fi
