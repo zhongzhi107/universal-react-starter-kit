0a54a3a6a56f3771e737c4e2a80c9dfa7fc17d9d
zhongzhi107 <zhong.zhi@163.com>
2017-03-02 10:32:35 +0800

    ci(build): Build scripts for jenkins

---
+++ scripts/build.client.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# 该文件是前端编译使用的脚本，后端工程不需要
+
+export PATH=/usr/local/n/versions/node/6.2.1/bin:$PATH
+
+if [ ! -n "$1" ] ;then
+  echo 参数错误
+  echo usage: build.sh cdn_root
+else
+  echo hostname: `hostname`
+  echo node version: `node -v`
+  echo npm version: `npm -v`
+  echo CDN_ROOT: $1
+  # npm-cache install --cacheDirectory /home/q/prj/npm/fe/dzs_touch --clearInvalidCache true npm --registry http://registry.npm.corp.qunar.com --disturl=https://npm.taobao.org/dist --sass-binary-site=http://npm.taobao.org/mirrors/node-sass --production
+  CDN_ROOT=$1 npm run build
+fio
