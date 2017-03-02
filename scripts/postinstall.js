b17ffe9d1d512675271e42d710c5c7054e367336
zhongzhi107 <zhong.zhi@163.com>
2017-03-02 13:01:21 +0800

    chore: Add debug output

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,6 +3,8 @@ import { execSync } from 'child_process';
 
 // run dll only in local machine
 const isLocal = !/^l-/.test(hostname);
+console.log(hostname);
+console.log('isLocal: ', isLocal);
 if (isLocal) {
   console.log(execSync('npm run dll').toString());
 }
