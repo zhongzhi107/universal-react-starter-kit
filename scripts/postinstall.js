89193ddc26f409f1a58dfd32b4e91b66b4ec4e80
zhongzhi107 <zhong.zhi@163.com>
2017-03-02 13:05:38 +0800

    fix(build): os.hostname syntax error

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,10 +1,9 @@
-import { hostname } from 'os';
+import os from 'os';
 import { execSync } from 'child_process';
 
 // run dll only in local machine
+const hostname = os.hostname();
 const isLocal = !/^l-/.test(hostname);
-console.log(hostname);
-console.log('isLocal: ', isLocal);
 if (isLocal) {
   console.log(execSync('npm run dll').toString());
 }
