b0bfb21820bae20a5fb4a856d332b3361257a7e8
zhongzhi107 <zhong.zhi@163.com>
2017-03-03 17:58:41 +0800

    ci: 修改build.sh参数形式

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,19 +3,14 @@
 
 export PATH=/usr/local/n/versions/node/6.2.1/bin:$PATH
 
-if [ ! -n "$1" ] ;then
-  echo "参数错误"
-  echo "usage: build.client.sh <cdn_root>"
-else
-  echo hostname: `hostname`
-  echo node version: `node -v`
-  echo npm version: `npm -v`
-  # npm-cache install --cacheDirectory /home/q/prj/npm/fe/dzs_touch --clearInvalidCache true npm --registry http://registry.npm.corp.qunar.com --disturl=https://npm.taobao.org/dist --sass-binary-site=http://npm.taobao.org/mirrors/node-sass --production
+echo hostname: `hostname`
+echo node version: `node -v`
+echo npm version: `npm -v`
+# npm-cache install --cacheDirectory /home/q/prj/npm/fe/dzs_touch --clearInvalidCache true npm --registry http://registry.npm.corp.qunar.com --disturl=https://npm.taobao.org/dist --sass-binary-site=http://npm.taobao.org/mirrors/node-sass --production
 
-  # Jenkins 会自动为前端工程安装好依赖包，后端工程需要手动装
-  if [ ! -d "node_modules" ]; then
-    npm install --registry http://registry.npm.corp.qunar.com --disturl=https://npm.taobao.org/dist --sass-binary-site=http://npm.taobao.org/mirrors/node-sass --production
-  fi
-
-  npm run build
+# Jenkins 会自动为前端工程安装好依赖包，后端工程需要手动装
+if [ ! -d "node_modules" ]; then
+  npm install --registry http://registry.npm.corp.qunar.com --disturl=https://npm.taobao.org/dist --sass-binary-site=http://npm.taobao.org/mirrors/node-sass --production
 fi
+
+npm run build
