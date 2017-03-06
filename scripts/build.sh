706c3be91cdd01a4ae8857c0fe0e80623e363094
zhongzhi107 <zhong.zhi@163.com>
2017-03-06 10:06:56 +0800

    ci: Debug

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,7 @@ echo node version: `node -v`
 echo npm version: `npm -v`
 # npm-cache install --cacheDirectory /home/q/prj/npm/fe/dzs_touch --clearInvalidCache true npm --registry http://registry.npm.corp.qunar.com --disturl=https://npm.taobao.org/dist --sass-binary-site=http://npm.taobao.org/mirrors/node-sass --production
 
+echo "NODE_ENV: ${NODE_ENV}"
 # Jenkins 会自动为前端工程安装好依赖包，后端工程需要手动装
 if [ ! -d "node_modules" ]; then
   npm install --registry http://registry.npm.corp.qunar.com --disturl=https://npm.taobao.org/dist --sass-binary-site=http://npm.taobao.org/mirrors/node-sass --production
