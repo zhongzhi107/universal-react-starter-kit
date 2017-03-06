07cf0aa02986c03044fbbb78e4b4d678c9ff27e6
zhongzhi107 <zhong.zhi@163.com>
2017-03-06 16:48:56 +0800

    fix: Enable dll plugin and check manifest file when yarn run serve

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,17 +1,17 @@
-// import os from 'os';
+import os from 'os';
 import { readFileSync, writeFileSync } from 'fs';
 import path from 'path';
-// import { execSync } from 'child_process';
+import { execSync } from 'child_process';
 
 // run dll only in local machine
-// const hostname = os.hostname();
-// const isLocal = !/^l-/.test(hostname);
-//
-// if (isLocal) {
-//   console.log(execSync('npm run dll').toString());
-// } else {
-//   console.log('npm run dll ... skipped');
-// }
+const hostname = os.hostname();
+const isLocal = !/^l-/.test(hostname);
+
+if (isLocal) {
+  console.log(execSync('npm run dll').toString());
+} else {
+  console.log('npm run dll ... skipped');
+}
 
 // Copy .env
 const { NODE_ENV = 'local' } = process.env;
